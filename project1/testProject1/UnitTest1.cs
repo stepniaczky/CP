22cf027fab30099eebf1aee88263b1d3131fcbbb
236636 Jakub Ramisz <236636@edu.p.lodz.pl>
2022-03-23 20:21:43 +0100

    Add example program and tests

---
+++ project1/testProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+using project1;
+
+namespace testProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            float result = Class1.Add(3, 4);
+            Assert.Equal<float>(7, result);
+        }
+    }
+}
