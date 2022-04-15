e419bb38c9a871b439b66ac7b0a182caa71a5acf
236636 Jakub Ramisz <236636@edu.p.lodz.pl>
2022-04-15 20:44:25 +0200

    Add ball class

---
+++ CP/Logic/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Logic
+{
+    public class Ball
+    {
+        private int x;
+        private int y;
+        private int radius;
+
+        public Ball(int x, int y, int radius)
+        {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+
+        public int Radius { get => radius; set => radius = value; }
+
+        public int Y { get => y; set => y = value; }
+
+        public int X { get => x; set => x = value; }
+    }
+}
