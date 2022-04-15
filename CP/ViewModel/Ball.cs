0da799d03870c5e7cad33f3f944c8da35ff63cc6
236636 Jakub Ramisz <236636@edu.p.lodz.pl>
2022-04-15 19:57:08 +0200

    Add ball class

---
+++ CP/ViewModel/Ball.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ViewModel
+{
+    public class Ball
+    {
+        private int x;
+        private int y;
+
+        public Ball(int x, int y)
+        {
+            this.x = x;
+
+        }
+
+        
+    }
+}
