255387b3e344c5c76d8bf55818962389325cf5de
236636 Jakub Ramisz <236636@edu.p.lodz.pl>
2022-04-15 22:10:02 +0200

    Add tests for ball class

---
+++ CP/LogicTests/BallTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+using System.Drawing;
+
+namespace LogicTests;
+
+[TestClass]
+public class BallTests
+{
+    static int testX = 10;
+    static int testY = 7;
+    static int testRadius = 2;
+    Ball testBall = new Ball(testX, testY, testRadius);
+
+    [TestMethod]
+    public void TestConstructor()
+    {
+        Assert.AreEqual(testX, testBall.Center.X);
+        Assert.AreEqual(testY, testBall.Center.Y);
+        Assert.AreEqual(testRadius, testBall.Radius);
+    }
+
+    [TestMethod]
+    public void TestMove()
+    {
+        int testXOffset = 5;
+        int testYOffset = 7;
+
+        testBall.Move(testXOffset, testYOffset);
+
+        Assert.AreEqual(testX + testXOffset, testBall.Center.X);
+        Assert.AreEqual(testY + testYOffset, testBall.Center.Y);
+    }
+}
