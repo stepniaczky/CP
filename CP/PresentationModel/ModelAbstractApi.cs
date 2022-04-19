c9e405bcecfb0ca5fdd385d99b134b7d0273ad89
236636 Jakub Ramisz <236636@edu.p.lodz.pl>
2022-04-19 18:07:16 +0200

    Add ModelAbstractApi class

---
+++ CP/PresentationModel/ModelAbstractApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Logic;
+
+namespace PresentationModel
+{
+    public abstract class ModelAbstractApi
+    {
+        public abstract int Radius { get; }
+
+
+        public static ModelAbstractApi CreateApi()
+        {
+            return new ModelApi();
+        }
+    }
+
+    internal class ModelApi : ModelAbstractApi
+    {
+        public override int Radius => 100;
+    }
+}
