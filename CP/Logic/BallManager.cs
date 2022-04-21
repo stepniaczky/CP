df8fe94c6cffb0226a67ac570b98d76ec3bb054a
236636 Jakub Ramisz <236636@edu.p.lodz.pl>
2022-04-21 19:00:25 +0200

    Add BallManager class to logic layer

---
+++ CP/Logic/BallManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Data;
+using System.Drawing;
+
+namespace Logic
+{
+	internal class BallManager : LogicApi
+	{
+		private readonly int _width;
+		private readonly int _height;
+		private readonly int _radius;
+		private List<DataApi> _balls = new List<DataApi>();
+
+        public override int Width { get => _width; }
+		public override int Height { get => _height; }
+		public override int Radius { get => _radius; }
+		public override List<DataApi> Balls { get => _balls; }
+		
+
+        public BallManager(int width, int height, int radius)
+		{
+			_width = width;
+			_height = height;
+			_radius = radius;
+		}
+
+		public override void CreateBalls(int amount)
+        {
+			Random random = new Random();
+
+			for (int i = 0; i < amount; i++)
+            {
+				Point motionDirection = new Point(random.Next(-1, 2), random.Next(-1, 2));
+				int x = random.Next(_radius, _width - _radius);
+				int y = random.Next(_radius, _height - _radius);
+
+				DataApi ball = DataApi.CreateBall(x, y, _radius);
+				ball.MotionDirection = motionDirection;
+				_balls.Add(ball);
+            }
+        }
+	}
+}
--- CP/Logic/LogicApi.cs
+++ CP/Logic/LogicApi.cs
@@ -1,52 +1,22 @@
 ﻿using System.Drawing;
 using System;
+using System.Collections.Generic;
+using Data;
 
 namespace Logic
 {
     public abstract class LogicApi
     {
+        public abstract List<DataApi> Balls { get; }
+        public abstract int Width { get; }
+        public abstract int Height { get; }
         public abstract int Radius { get; }
-        public abstract Point Center { get; }
-        public abstract Point MotionDirection { get; set; }
 
-        public abstract void Move(int leftBound, int rightBound, int bottomBound, int topBound);
-
-
-        public static LogicApi CreateApi(int x, int y, int radius)
+        public static LogicApi CreateApi(int width, int height, int radius) 
         {
-            return new Ball(x, y, radius);
+            return new BallManager(width, height, radius);
         }
 
-        internal class Ball : LogicApi
-        {
-            private readonly int _radius;
-            private Point _center;
-            private Point _motionDirection;
-            private Random random = new Random();
-
-            public override int Radius { get => _radius; }
-            public override Point Center { get => _center; }
-            public override Point MotionDirection { get => _motionDirection; set => _motionDirection = value; }
-
-            public Ball(int x, int y, int radius)
-            {
-                _center = new Point(x, y);
-                _radius = radius;
-            }
-
-            public override void Move(int leftBound, int rightBound, int bottomBound, int topBound)
-            {
-
-                while (!(leftBound <= (_center.X + _motionDirection.X - _radius) &&
-                                rightBound >= (_center.X + _motionDirection.X + _radius) &&
-                                bottomBound <= (_center.Y + _motionDirection.Y - _radius) &&
-                                topBound >= (_center.Y + _motionDirection.Y + _radius)))
-                {
-                    _motionDirection = new Point(random.Next(-1, 2), random.Next(-1, 2));
-                }
-
-                _center.Offset(_motionDirection);
-            }
-        }
+        public abstract void CreateBalls(int amount);
     }
 }
