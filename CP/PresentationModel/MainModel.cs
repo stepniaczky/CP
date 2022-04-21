de26e4451af37ed686bc41664cbf9c1ed004848b
236659 Mikołaj Stępniak <236659@edu.p.lodz.pl>
2022-04-21 18:08:36 +0200

    Model Abstract Api - implementation

---
+++ CP/PresentationModel/MainModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Logic;
+
+namespace PresentationModel
+{
+    internal class ModelApi : ModelAbstractApi
+    {
+        private LogicApi LogicLayer = LogicApi.CreateApi();
+        private readonly int _width;
+        private readonly int _height;
+        public int Width { get =>  _width; } 
+        public int Height { get => _height; }
+
+        public override int Radius => 1/14 * Height;  //TODO responsiveness
+        public ModelApi() : this(LogicApi.CreateApi(Width, Height, Radius))
+        {
+        }
+
+        public ModelApi(LogicApi logicApi)
+        {
+            LogicLayer = logicApi;
+        }
+
+        public override void CreateBalls(int ballsNumber)
+        {
+            return _logicApi.CreateBalls();
+        }
+
+        public override List<object> GetBalls()
+        {
+            return _logicApi.GetBalls();
+        }
+
+        public override void ClearBalls()
+        {
+            return _logicApi.ClearBalls();
+        }
+    }
+}
