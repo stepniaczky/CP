fd87b286bc79ea7414863ec7874d410bd87f0f92
236659 Mikołaj Stępniak <236659@edu.p.lodz.pl>
2022-04-26 12:30:07 +0200

    Adding an IObserver/ISubject interface class

---
+++ CP/Logic/IObserver.cs
@@ -0,0 +1,7 @@
+﻿namespace Logic
+{
+    public interface IObserver
+    {
+        void Update(ISubject subject);
+    }
+}
---
+++ CP/Logic/ISubject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Logic
+{
+    public interface ISubject
+    {
+        List<Ball> Balls { get; }
+
+        void Attach(IObserver observer);
+        void Detach(IObserver observer);
+        void Notify();
+    }
+}
